e0aa95a20828067413b390a34cfa72d12624bd95
조현우 <a01058830723@gmail.com>
2025-12-21 22:24:41 +0900

    fix: student error.tsx에서 중복 컨테이너 스타일링 제거
    - getContainerClass import 및 사용 제거
    - ErrorPage 컴포넌트가 이미 컨테이너 스타일링 포함
    - 다른 error.tsx 파일들과 동일한 구조로 통일
    - Next.js 16 React Client Manifest 에러 해결

--- app/(student)/error.tsx
+++ app/(student)/error.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import ErrorPage from "@/components/errors/ErrorPage";
-import { getContainerClass } from "@/lib/constants/layout";
 
-interface ErrorProps {
+type ErrorProps = {
   error: Error & { digest?: string };
   reset: () => void;
-}
+};
 
 export default function Error({ error, reset }: ErrorProps) {
-  return (
-    <div className={getContainerClass("DASHBOARD", "lg")}>
-      <ErrorPage error={error} reset={reset} role="student" />
-    </div>
-  );
+  return <ErrorPage error={error} reset={reset} role="student" />;
 }
